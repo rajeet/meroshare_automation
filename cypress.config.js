2fd081207b72e16eb6ea362c1b58b4cb684c2cf0
rajeet <mangratirajit@gmail.com>
2023-12-14 12:32:02 +0545

    update

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,7 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
     baseUrl:"https://meroshare.cdsc.com.np/", 
+    projectId: "7mq8wz",
     env:{...process.env},
   },
 });
