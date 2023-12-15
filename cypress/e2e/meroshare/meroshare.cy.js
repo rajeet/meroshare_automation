faf54e941438b2c65809d4f44b7bb6ac61d4e7e8
rajeet <mangratirajit@gmail.com>
2023-12-15 11:10:16 +0545

    updated automation

--- cypress/e2e/meroshare/meroshare.cy.js
+++ cypress/e2e/meroshare/meroshare.cy.js
@@ -12,25 +12,25 @@ describe('Meroshare Automation', () => {
   describe('Meroshare automation', () => {
     it('Apply for share', () => {
       // intercept the request for check
-      cy.intercept("POST", "https://webbackend.cdsc.com.np/api/meroShare/companyShare/applicableIssue/", 
-      // {
-      //   statusCode: 200,
-      //   body: {
-      //     "object": [{
-      //       "action": "edit",
-      //       "companyShareId": 615,
-      //       "subGroup": "For General Public",
-      //       "scrip": "HREL",
-      //       "companyName": "Himalayan Re-insurance Limited",
-      //       "shareTypeName": "IPO",
-      //       "shareGroupName": "Ordinary Shares",
-      //       "statusName": "CREATE_APPROVE",
-      //       "issueOpenDate": "Dec 13, 2023 10:00:00 AM",
-      //       "issueCloseDate": "Dec 17, 2023 5:00:00 PM"
-      //     }],
-      //     "totalCount": 0
-      //   }
-      // }
+      cy.intercept("POST", "https://webbackend.cdsc.com.np/api/meroShare/companyShare/applicableIssue/",
+        // {
+        //   statusCode: 200,
+        //   body: {
+        //     "object": [{
+        //       "action": "edit",
+        //       "companyShareId": 615,
+        //       "subGroup": "For General Public",
+        //       "scrip": "HREL",
+        //       "companyName": "Himalayan Re-insurance Limited",
+        //       "shareTypeName": "IPO",
+        //       "shareGroupName": "Ordinary Shares",
+        //       "statusName": "CREATE_APPROVE",
+        //       "issueOpenDate": "Dec 13, 2023 10:00:00 AM",
+        //       "issueCloseDate": "Dec 17, 2023 5:00:00 PM"
+        //     }],
+        //     "totalCount": 0
+        //   }
+        // }
       ).as("applicableshare");
 
 
@@ -39,16 +39,16 @@ describe('Meroshare Automation', () => {
       }).as("active");
 
       cy.intercept({
-        url:"https://webbackend.cdsc.com.np/api/meroShare/applicantForm/share/apply"
+        url: "https://webbackend.cdsc.com.np/api/meroShare/applicantForm/share/apply"
       }).as('apply_share');
 
       // intercept end.
 
-      console.log(result);
-
       cy.visit('/');
       // username and password input
-      cy.get('#password').clear().type(result.password, {log:false})
+      cy.get('#password').clear().type(result.password, {
+        log: false
+      })
       cy.get('#username').clear().type(result.username).should('have.value', result.username)
 
       // select the DP
@@ -69,6 +69,7 @@ describe('Meroshare Automation', () => {
       cy.wait("@applicableshare").then((resp) => {
         const response = resp.response.body.object;
         response.map((data) => {
+          console.log(data)
           if (!data.action) {
             cy.get(".company-list .company-name span[tooltip='Company Name']").contains(data.companyName).should('be.visible');
             cy.get(".company-list").contains(data.companyName).get(".action-buttons button").click();
@@ -77,6 +78,17 @@ describe('Meroshare Automation', () => {
               console.log(response);
               if (response.shareTypeName === "IPO" && response.shareGroupName === "Ordinary Shares" && response.subGroup === "For General Public" && response.sharePerUnit <= result.maximum_share_price) {
                 console.log("ready to buy");
+
+                cy.visit('/');
+
+                // after login goto my asba page 
+                cy.get('li.nav-item').contains("My ASBA", {
+                  matchCase: false
+                }).click();
+
+                cy.get(".company-list .company-name span[tooltip='Company Name']").contains(data.companyName).should('be.visible');
+                cy.get(".company-list").contains(data.companyName).get(".action-buttons button").click();
+
                 // select the bank and confirm for buy.
                 if (result.bankName == "" || !result.bankName) {
                   cy.get("#selectBank").select(1);
@@ -89,7 +101,9 @@ describe('Meroshare Automation', () => {
 
 
                 // type crn number 
-                cy.get("#crnNumber").type(result.crn, {log: false});
+                cy.get("#crnNumber").type(result.crn, {
+                  log: false
+                });
 
                 // agree terms
                 cy.get('#disclaimer').check().should("be.checked");
@@ -100,10 +114,12 @@ describe('Meroshare Automation', () => {
                 }).click();
 
                 // input transaction pin
-                cy.get("#transactionPIN").type(result.transactionPin, {log: false})
+                cy.get("#transactionPIN").type(result.transactionPin, {
+                  log: false
+                })
                 cy.get("button").contains("Apply").click();
 
-                cy.wait('@apply_share').then((resp)=>{
+                cy.wait('@apply_share').then((resp) => {
                   expect(resp.response.statusCode).to.be.eq(201);
                 })
 
